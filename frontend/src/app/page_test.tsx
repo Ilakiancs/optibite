f6cca98f2ad78f101709d26116cfabe38908c60c
Ilakian Puvanendra <ilakianpuvanendra@Ilakians-MacBook-Pro.local>
2025-08-19 23:02:03 +0530

    Add test page for feature development

---
+++ frontend/src/app/page_test.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>OptiBite - Working!</h1>
+      <p>This is a test to see if React is working properly.</p>
+    </div>
+  );
+}
